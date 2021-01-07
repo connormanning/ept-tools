0db2369059930977c859f31530baf6cbd4b295c9
Connor Manning <connor@hobu.co>
2021-01-07 14:10:55 -0600

    Fix extra bytes handling.

--- src/ept/data-type/laszip/index.ts
+++ src/ept/data-type/laszip/index.ts
@@ -1,5 +1,7 @@
 import Module from 'lib/laz-perf.asm'
 
+import { Schema } from 'ept'
+
 import { View } from '../../view'
 
 import * as Format from './format'
@@ -15,27 +17,32 @@ export const Laszip = { view }
 // respective dimensions: they are combined as the single dimension "Flag".
 function view(input: Buffer): View.Readable {
   const header = Header.parse(input)
-  const { pointCount, pointSize } = header
+  const { pointCount } = header
 
   const laszip = new Module.LASZip()
   const filePointer = Module._malloc(input.length)
-  const dataPointer = Module._malloc(pointSize)
+  const dataPointer = Module._malloc(header.pointSize)
 
   try {
     Module.HEAPU8.set(input, filePointer)
     laszip.open(filePointer, input.length)
 
-    const point = Buffer.from(Module.HEAPU8.buffer, dataPointer, pointSize)
-
-    const length = pointSize * pointCount
-    const output = Buffer.alloc(length)
-    for (let pos = 0; pos < length; pos += pointSize) {
-      // Decompress each point and copy it from the Module heap to normal memory.
-      laszip.getPoint(dataPointer)
-      point.copy(output, pos, 0, pointSize)
-    }
-
+    // Note that the point size within the file itself may be larger than the
+    // schema that we create here because there may be extra-bytes (which we
+    // will ignore).  So when we unpack the point into our one-point temporary
+    // buffer we need the larger size, but when we copy the point content into
+    // our localized buffer we will omit these trailing extra-bytes.
     const schema = Format.create(header)
+    const corePointSize = Schema.pointSize(schema)
+    const point = Buffer.from(Module.HEAPU8.buffer, dataPointer, corePointSize)
+
+    const length = corePointSize * pointCount
+    const output = Buffer.alloc(length)
+    for (let pos = 0; pos < length; pos += corePointSize) {
+      // Decompress each point and copy it from the Module heap to our buffer.
+      laszip.getPoint(dataPointer)
+      point.copy(output, pos, 0, corePointSize)
+    }
 
     return View.Readable.create(output, schema)
   } finally {
